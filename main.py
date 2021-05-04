440315dcb73a95939d53655c8f07a2d24bc9e7f8
Romildo Fonseca <cantalice@gmail.com>
2021-05-04 02:03:53 -0300

    less php

--- main.py
+++ main.py
@@ -14,7 +14,7 @@
 
 # [START gae_python38_app]
 # [START gae_python3_app]
-from flask import Flask
+from flask import Flask, render_template
 
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
