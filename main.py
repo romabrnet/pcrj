641af9d2b198e38fa5de549480af0ed6b9dcffdc
Romildo Fonseca <cantalice@gmail.com>
2021-05-04 02:20:20 -0300

    mapa

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START gae_python38_app]
-# [START gae_python3_app]
 from flask import Flask, render_template
 
+# Image
+import io
+from PIL import Image
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
@@ -27,10 +14,30 @@ def home():
     return render_template('index.html')
 
 
+######################
+### Image Handling ###
+######################
+
+@app.route('/local-api/image', methods=['POST'])
+def postAPI():
+
+    try:
+        data = request.files.get('image').read()
+
+        image = Image.open(io.BytesIO(data))
+        image.save('images/temp.png')
+
+        time.sleep(2)
+
+        return jsonify({'response': "Done Saving Image"})
+
+    except Exception as e:
+        traceback.print_exc(chain=False)
+        return jsonify({"error": repr(e)})
+
+
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_python3_app]
-# [END gae_python38_app]
